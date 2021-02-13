e0021f8146d74006e37893b852b223a1f0d73eca
hlovatt <howard.lovatt@gmail.com>
2021-02-13 15:58:17 +1100

    Run traffic light statmach on PyBoard.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""uPython Traffic Light State Machine"""
+"""uPython Traffic Light State Machine."""
 
 __author__ = "Howard C Lovatt"
 __copyright__ = "Howard C Lovatt, 2021 onwards."
@@ -7,48 +7,91 @@ __repository__ = "https://github.com/hlovatt/upythonstatmechex"
 __description__ = "``statmach`` traffic light example for MicroPython."
 __version__ = "0.0.0"  # Version set by https://github.com/hlovatt/tag2ver
 
-from statmach import StateWithValue, Machine
+from statmach import State, Machine
+from pyb import LED, Timer, wfi, Switch
+from micropython import alloc_emergency_exception_buf, schedule
+
+alloc_emergency_exception_buf(200)
 
 
 def main():
-    class Inputs:  # 1. The inputs.
-        RED_TIMEOUT = 0
-        AMBER_TIMEOUT = 1
-        GREEN_TIMEOUT = 2
-        ERROR = 3
+    print('Traffic lights running ...')
 
-    class Outputs:  # 2. The outputs.
-        RED = 0
-        AMBER = 1
-        GREEN = 2
-        FLASHING_RED = 3
+    class Events:
+        RED_TIMEOUT = 1
+        AMBER_TIMEOUT = 2
+        GREEN_TIMEOUT = 3
+        ERROR = 4
+        START = 5
 
-    flashing_red = StateWithValue(ident='flashing_red', value=Outputs.FLASHING_RED)  # 3. The states.
-    red = StateWithValue(ident='red', value=Outputs.RED)
-    amber = StateWithValue(ident='amber', value=Outputs.AMBER)
-    green = StateWithValue(ident='green', value=Outputs.GREEN)
+    start = State(ident='start')  # Special start state to allow for initialization before operation.
 
-    red.actions[Inputs.RED_TIMEOUT] = green.action  # 4a. The *state* actions.
-    green.actions[Inputs.GREEN_TIMEOUT] = amber.action
-    amber.actions[Inputs.AMBER_TIMEOUT] = red.action
+    timer0 = 10
 
-    with Machine(initial_state=red) as machine:  # 5. The machine.
-        machine.actions[Inputs.RED_TIMEOUT] = flashing_red.action  # 4b. The *machine* actions.
-        machine.actions[Inputs.AMBER_TIMEOUT] = flashing_red.action
-        machine.actions[Inputs.GREEN_TIMEOUT] = flashing_red.action
-        machine.actions[Inputs.ERROR] = flashing_red.action
+    class FlashingRed(State):  # Special fault state that should never exit.
+        def __init__(self):
+            super().__init__(ident='error')
+            self.timer = Timer(timer0 + 4)
+            self.led = LED(1)
 
-        assert machine.state is red
-        assert machine.fire(event=Inputs.RED_TIMEOUT) is Outputs.GREEN  # 6. Fire events and obtain outputs.
-        assert machine.state is green
-        assert machine.fire(event=Inputs.GREEN_TIMEOUT) is Outputs.AMBER
-        assert machine.state is amber
-        assert machine.fire(event=Inputs.AMBER_TIMEOUT) is Outputs.RED
-        assert machine.state is red
-        assert machine.fire(event=Inputs.AMBER_TIMEOUT) is Outputs.FLASHING_RED
-        assert machine.state is flashing_red
-        assert machine.fire(event=Inputs.ERROR) is Outputs.FLASHING_RED
-        assert machine.state is flashing_red
+            # noinspection PyUnusedLocal
+            def toggle_with_arg(not_used):  # Toggle func that accepts an arg, because ``schedule`` *needs* an arg.
+                self.led.toggle()
+
+            self.led_tog_ref = toggle_with_arg  # Store the function reference locally to avoid allocation in interrupt.
+
+        def __enter__(self):
+            self.timer.init(freq=2, callback=lambda _: schedule(self.led_tog_ref, None))
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.led.off()
+            self.timer.deinit()
+
+    flashing_red = FlashingRed()
+
+    traffic_lights = Machine(initial_state=start)  # The traffic light machine.
+    traffic_lights.actions[Events.RED_TIMEOUT] = (flashing_red, None)  # Catch anything unexpected.
+    traffic_lights.actions[Events.AMBER_TIMEOUT] = (flashing_red, None)
+    traffic_lights.actions[Events.GREEN_TIMEOUT] = (flashing_red, None)
+    traffic_lights.actions[Events.ERROR] = (flashing_red, None)
+    traffic_lights.actions[Events.START] = (flashing_red, None)
+
+    tl_fire_ref = traffic_lights.fire  # Store the function reference locally to avoid allocation in interrupt.
+    error = Switch()
+    error.callback(lambda: schedule(tl_fire_ref, Events.ERROR))
+
+    class LEDState(State):  # Output is determined by ``__enter__`` and ``__exit__`` (common in embedded machines).
+        def __init__(self, led_num, time_on, event):
+            super().__init__(ident=led_num)  # Use the LED num as the ident.
+            self.led = LED(self.ident)  # The LED to use.
+            self.timer = Timer(timer0 + self.ident)  # The timer to use.
+            self.timeout = time_on  # Time to wait before firing event.
+            self.event = event  # Event to fire at end of time_on.
+
+        def __enter__(self):
+            self.led.on()
+            self.timer.init(freq=1 / self.timeout, callback=lambda _: schedule(tl_fire_ref, self.event))
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.led.off()
+            self.timer.deinit()
+            return False
+
+    red = LEDState(led_num=1, time_on=3, event=Events.RED_TIMEOUT)
+    green = LEDState(led_num=2, time_on=3, event=Events.GREEN_TIMEOUT)
+    amber = LEDState(led_num=3, time_on=0.5, event=Events.AMBER_TIMEOUT)
+
+    red.actions[Events.RED_TIMEOUT] = (green, None)
+    green.actions[Events.GREEN_TIMEOUT] = (amber, None)
+    amber.actions[Events.AMBER_TIMEOUT] = (red, None)
+    start.actions[Events.START] = (red, None)
+
+    with traffic_lights:
+        _ = traffic_lights.fire(event=Events.START)  # Start the machine once all the setup is complete.
+        while True:  # Keep running timers (and other peripherals), but otherwise do nothing.
+            wfi()
 
 
 if __name__ == '__main__':
