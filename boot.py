47e285611abc36f18142f955708598c56565e405
hlovatt <howard.lovatt@gmail.com>
2021-02-13 16:12:43 +1100

    "1st running version using early statmach."

--- boot.py
+++ boot.py
@@ -1,6 +1,13 @@
 # boot.py -- run on boot-up
 # can run arbitrary Python, but best to keep it minimal
 
+__author__ = "Howard C Lovatt"
+__copyright__ = "Howard C Lovatt, 2021 onwards."
+__license__ = "MIT https://opensource.org/licenses/MIT."
+__repository__ = "https://github.com/hlovatt/upythonstatmechex"
+__description__ = "``statmach`` traffic light example for MicroPython."
+__version__ = "0.0.3"  # Version set by https://github.com/hlovatt/tag2ver
+
 import machine
 import pyb
 pyb.country('AU') # ISO 3166-1 Alpha-2 code, eg US, GB, DE, AU
