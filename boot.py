52d462fe08a46f2b0d5885b0edb3e239a826b32d
hlovatt <howard.lovatt@gmail.com>
2021-02-19 10:22:00 +1100

    "Fixes mech, incorrect, instead of mach, correct, typo; try 2."

--- boot.py
+++ boot.py
@@ -4,9 +4,9 @@
 __author__ = "Howard C Lovatt"
 __copyright__ = "Howard C Lovatt, 2021 onwards."
 __license__ = "MIT https://opensource.org/licenses/MIT."
-__repository__ = "https://github.com/hlovatt/upythonstatmechex"
+__repository__ = "https://github.com/hlovatt/upythonstatmachex"
 __description__ = "``statmach`` traffic light example for MicroPython."
-__version__ = "1.0.2"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "1.0.3"  # Version set by https://github.com/hlovatt/tag2ver
 
 import machine
 import pyb
